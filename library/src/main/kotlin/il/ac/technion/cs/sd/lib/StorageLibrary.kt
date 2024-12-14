619ee342fb77b20489abaa0f9960237f24cdb8bb
Yonathan Shterenberg <yonathan.shterenberg@gmail.com>
2024-12-14 18:36:38 +0200

    Implemented StorageLibrary setup (with tests)

--- library/src/main/kotlin/il/ac/technion/cs/sd/lib/StorageLibrary.kt
+++ library/src/main/kotlin/il/ac/technion/cs/sd/lib/StorageLibrary.kt
@@ -2,6 +2,7 @@ package il.ac.technion.cs.sd.lib
 
 import il.ac.technion.cs.sd.grades.external.LineStorage
 
+
 /**
  * Implement your library here. Feel free to change the class name,
  * but note that if you choose to change the class name,
@@ -9,7 +10,28 @@ import il.ac.technion.cs.sd.grades.external.LineStorage
  * and in GradesReader.kt.
  */
 object StorageLibrary {
-    fun setup(studentGrades: Map<Int, Int>) {}
+    fun setup(studentGrades: Map<Int, Int>) {
+        val sortedStudentGrades = studentGrades.toSortedMap()
+
+        sortedStudentGrades
+            .forEach { s ->
+                if (s.key.toString().length > 9 || s.key < 0) {
+                    throw Exception(
+                        "Invalid student id: " + s.key +
+                                ". Should be positive and up to 9 digits"
+                    )
+                }
+                if (s.value.toString().length > 3 || s.value < 0)
+                {
+                    throw Exception(
+                        "Invalid student grade: " + s.value +
+                                ". Should be positive and up to 3 digits"
+                    )
+                }
+                LineStorage.appendLine(s.key.toString() + " " + s.value.toString())
+            }
+    }
+
     fun getGrade(id: Int): Int? {
         TODO()
     }
--- library/src/test/kotlin/il/ac/technion/cs/sd/lib/StorageLibraryTest.kt
+++ library/src/test/kotlin/il/ac/technion/cs/sd/lib/StorageLibraryTest.kt
@@ -1,8 +1,65 @@
 package il.ac.technion.cs.sd.lib
 
+import il.ac.technion.cs.sd.grades.external.LineStorage
 import org.junit.jupiter.api.*
 
 /** Use this class to test your library implementation */
 class StorageLibraryTest {
 
+    @BeforeEach
+    fun `reset line storage`(){
+        LineStorage.reset()
+    }
+
+    @Test
+    fun `setup includes students in a sorted order`(){
+        val studentMap : MutableMap<Int, Int> = mutableMapOf()
+        studentMap[214762718] = 100
+        studentMap[214762338] = 1
+        studentMap[214762339] = 69
+        studentMap[214] = 2
+        studentMap[762339] = 3
+        StorageLibrary.setup(studentMap)
+
+        val sortedStudents = studentMap.toSortedMap()
+        for ((i, studentId) in sortedStudents.keys.withIndex())
+        {
+            Assertions.assertEquals(
+                studentId.toString() + " " + sortedStudents[studentId], LineStorage.read(i))
+        }
+    }
+
+    @Test
+    fun `only the last grade is saved`(){
+        // no need to check with current map architecture.
+        // Map doesn't allow two values for the same id
+    }
+
+    @Test
+    fun `throws on invalid id - negative`(){
+        val studentMap : MutableMap<Int, Int> = mutableMapOf()
+        studentMap[-1] = 2
+        assertThrows<Exception> { StorageLibrary.setup(studentMap) }
+    }
+
+    @Test
+    fun `throws on invalid id - more than 9 digits`(){
+        val studentMap : MutableMap<Int, Int> = mutableMapOf()
+        studentMap[1234567891] = 2
+        assertThrows<Exception> { StorageLibrary.setup(studentMap) }
+    }
+
+    @Test
+    fun `throws on invalid grade - negative`(){
+        val studentMap : MutableMap<Int, Int> = mutableMapOf()
+        studentMap[123] = -1
+        assertThrows<Exception> { StorageLibrary.setup(studentMap) }
+    }
+
+    @Test
+    fun `throws on invalid grade - more than 3 digits`(){
+        val studentMap : MutableMap<Int, Int> = mutableMapOf()
+        studentMap[123] = 1234
+        assertThrows<Exception> { StorageLibrary.setup(studentMap) }
+    }
 }
